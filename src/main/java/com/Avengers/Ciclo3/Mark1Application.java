d12fba5406d915a7ff17ccf36ac1461b34f0de5c
Alejandro Huertas Rojas <alejandro.huertasrojas@gmail.com>
2022-09-08 22:35:16 -0500

    Primer Commit

---
+++ src/main/java/com/Avengers/Ciclo3/Mark1Application.java
@@ -0,0 +1,13 @@
+package com.Avengers.Ciclo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mark1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mark1Application.class, args);
+	}
+
+}
